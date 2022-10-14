53d808c793ea74ad2b23a508ff3c4d90cef33551
FabioVascao <109177231+FabioVascao@users.noreply.github.com>
2022-10-14 11:04:11 -0300

    comentarios

--- script.js
+++ script.js
@@ -18,18 +18,18 @@ let listStudents = [ /* Cria um array com objetos dentro */
   }
 ]
 
-let media = 0;
+let media = 0; /* Declara a variável media que será usada na função */
 
-function result(listStudents) {
-  let media = (listStudents.nota1 + listStudents.nota2)/2
+function result(listStudents) { /* Executa a função na listStudents */
+  let media = (listStudents.nota1 + listStudents.nota2)/2 /* calcula a média */
   if (media >= 7) {
-  return (`A média do ${listStudents.name} é de ${media}. Parabéns ${listStudents.name}, você foi aprovado!`)
+  return (`A média do ${listStudents.name} é de ${media}. Parabéns ${listStudents.name}, você foi aprovado!`) /* Se media >= 7 faz isso */
 } else {
-  return (`A média do ${listStudents.name} é de ${media}. Não foi dessa vez ${listStudents.name}, você foi reprovado.`)
+  return (`A média do ${listStudents.name} é de ${media}. Não foi dessa vez ${listStudents.name}, você foi reprovado.`) /* Se media < 7 faz isso */
 }
 }
 
-for (let listStudent of listStudents) {
+for (let listStudent of listStudents) { /* Cria e propriedada listStudent dentro da listStudents e depois escreve */
   let mediaMessage = result(listStudent)
   alert(mediaMessage)
 }
